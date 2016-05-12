0c72481e80fdacdf63344c17d01b2d5202fdcf32
Astor-Bizard <astor.bizard@laposte.net>
2016-05-12 13:17:19 +0200

    Proprisation du code

--- Taluva/src/main/Tuile.java
+++ Taluva/src/main/Tuile.java
@@ -2,44 +2,59 @@ package main;
 
 public class Tuile {
 	
+	public enum Orientation{
+		GAUCHE,
+		DROITE;
+	}
 	private Case.Type [] t;
-	private int TYPE;	// 0 ou 1
+	private Orientation o;
 	
 	//		  	Indices :
 	//		     _	    _
 	//		   _/2\    /1\_
-	// Type 0 /0\_/    \_/3\ Type 1
+	// GAUCHE /0\_/    \_/3\ DROITE
 	//	 	  \_/4\    /5\_/
 	//		    \_/    \_/
 	
-	// Crée une tuile (type 0) avec le volcan à gauche, typeb en bas et typeh en haut.
+	// Crée une tuile (GAUCHE) avec le volcan à gauche, typeb en bas et typeh en haut.
 	Tuile(Case.Type typeb, Case.Type typeh){
 		t = new Case.Type[6];
-		TYPE = 0;
+		o = Orientation.GAUCHE;
 		t[0] = Case.Type.VOLCAN;
 		t[2] = typeh;
 		t[4] = typeb;
 	}
 	
 	private void swap_type(){
-		TYPE = 1-TYPE;
+		if(o == Orientation.GAUCHE) o = Orientation.DROITE;
+		else o = Orientation.GAUCHE;
+	}
+	
+	public Orientation getOrientation(){
+		return o;
 	}
 	
 	// Tourne la tuile d'1/6 de tour en sens horaire
 	public void tourner_horaire(){
-		for(int i=TYPE;i<5;i+=2){
+		int i;
+		if(o == Orientation.GAUCHE) i = 0;
+		else i = 1;
+		for(;i<5;i+=2){
 			t[i+1]=t[i];
 		}
-		if(TYPE == 1) t[0] = t[5];
+		if(o == Orientation.DROITE) t[0] = t[5];
 		swap_type();
 	}
 
 	// Tourne la tuile d'1/6 de tour en sens anti-horaire
 	public void tourner_anti_horaire(){
 		swap_type();
-		for(int i=TYPE;i<5;i+=2){
+		int i;
+		if(o == Orientation.GAUCHE) i = 0;
+		else i = 1;
+		for(;i<5;i+=2){
 			t[i]=t[i+1];
 		}
-		if(TYPE == 1) t[5] = t[0];
+		if(o == Orientation.DROITE) t[5] = t[0];
 	}
 }
