fd2c10c33a4eb4dc7b61e6839dcaf99f8a750545
lavvsont <lavvsont@127.0.0.1>
2016-05-12 12:58:25 +0200

    annul et redo à implémenter

--- Taluva/src/main/Moteur.java
+++ Taluva/src/main/Moteur.java
@@ -1,10 +1,16 @@
 package main;
+import java.util.ArrayList;
 
 public class Moteur {
 	private Terrain T;
+	private ArrayList<Terrain> annul;
+	private ArrayList<Terrain> redo;
 	
 	Moteur(Terrain T){
 		this.T = T;
+		annul = new ArrayList<Terrain>();
+		//annul.add(T.clone);
+		redo = new ArrayList<Terrain>();
 	}
 	
 	//
