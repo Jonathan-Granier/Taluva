97a4a93ddc9bdb9268a3aab4241b247eeed00335
Jonathan-Granier <jonathan.granier@wanadoo.fr>
2016-05-17 16:36:18 +0200

    Ajout des ecouteurs de souris

---
+++ Taluva/src/Ecouteur/EcouteurDeSourisPanneauInteraction.java
@@ -0,0 +1,87 @@
+
+
+package Ecouteur;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import main.Moteur;
+
+public class EcouteurDeSourisPanneauInteraction implements MouseListener, MouseMotionListener{
+	Moteur m;
+	
+	
+	public EcouteurDeSourisPanneauInteraction(Moteur m)
+	{
+		this.m = m;
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		switch (m.etat)
+		{
+			case DEBUT_DE_TOUR:
+				
+				/*
+				 * Si cliquerSurPioche
+				 * 		Mettre à jour les coups possible terrain;
+				 * 		Increment Etat;
+				 */
+				
+				
+				break;
+			case POSER_TUILE:
+				break;
+			case CONSTRUIRE_BATIMENT:
+				/*Si clique Sur Batiment Joueur 
+				 * 		SelectionBatiment
+				 */
+				
+				
+				break;
+			case FIN_DE_TOUR:
+				break;
+			default:
+				break;
+		}
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+	}	
+	
+
+	@Override
+	public void mouseMoved(MouseEvent e) {	
+	}
+	
+
+}
