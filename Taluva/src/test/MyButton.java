5f926dc6148cd0a57f961efb7594605d0f9b6d2e
Dimitri <Dimitri@Dimi-PC>
2016-05-19 11:04:36 +0200

    On peut poser des huttes et des tours

--- Taluva/src/test/MyButton.java
+++ Taluva/src/test/MyButton.java
@@ -2,20 +2,41 @@ package test;
 
 import org.lwjgl.util.vector.Vector2f;
 
+import entities.GraphicConstruction;
 import entities.GraphicConstruction.GraphicType;
 import gui.Button;
 
 public class MyButton extends Button {
 
 	public GraphicType type = GraphicType.NULL;
+	private String label;
+	private GraphicConstruction Construction;
 	
-	public MyButton(int textureId, Vector2f position, Vector2f dimension) {
+	public MyButton(int textureId, Vector2f position, Vector2f dimension,String label,GraphicConstruction Construction) {
 		super(textureId, position, dimension);
+		this.label = label;
+		this.Construction = Construction;
 	}
 
 	@Override
 	protected void action() {
-		type = GraphicType.TOWER;
+		if(label.equals("tower")){
+			type = GraphicType.TOWER;
+			Construction.setType(GraphicType.TOWER);
+		}
+		else if(label.equals("hut")){
+			type = GraphicType.HUT;
+			Construction.setType(GraphicType.HUT);
+		}
+		else if(label.equals("temple")){
+			type = GraphicType.TEMPLE;
+			Construction.setType(GraphicType.TEMPLE);
+		}
+		else{
+			type = GraphicType.NULL;
+			Construction.setType(GraphicType.NULL);
+		}
+		Construction.setObject3d();
 	}
 	
 	
