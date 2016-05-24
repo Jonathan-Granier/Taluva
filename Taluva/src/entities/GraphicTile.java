1928764d04a741634bef20451b0f85d325cc4f5c
Astor-Bizard <astor.bizard@laposte.net>
2016-05-24 11:04:32 +0200

    Retour en arriere d'un merge mysterieux qui a tout cassé

--- Taluva/src/entities/GraphicTile.java
+++ Taluva/src/entities/GraphicTile.java
@@ -120,6 +120,31 @@ public class GraphicTile {
 	public void setAngle(float angle) {
 		this.angle = angle;
 	}
+	
+	public void setAngle(){
+		switch(tile.get_Orientation_Volcan()){
+		case O:
+			object3d.setRotY(90);
+			break;
+		case S_O:
+			object3d.setRotY(150);
+			break;
+		case S_E:
+			object3d.setRotY(210);
+			break;
+		case E:
+			object3d.setRotY(270);
+			break;
+		case N_E:
+			object3d.setRotY(330);
+			break;
+		case N_O:
+			object3d.setRotY(30);
+			break;
+		default:
+			System.out.println("Rotate cannot be done");
+		}
+	}
 
 	public float getHeight() {
 		return height;
