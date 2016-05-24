66bf5813bd2765e7ca6a7f5f8859674f182b628d
bouvigab <bouvigab@127.0.0.1>
2016-05-24 17:20:52 +0200

    Merge finis.

--- Taluva/src/Moteur/Etat_de_jeu.java
+++ Taluva/src/Moteur/Etat_de_jeu.java
@@ -8,14 +8,14 @@ public class Etat_de_jeu {
 	@SuppressWarnings("unused")
 	private Terrain terrain;
 	private Joueur_Generique j1,j2,j_courant;
-	private Phase pdj;
+	private Phase_Jeu pdj;
 
-	public Etat_de_jeu(Terrain t, Joueur_Generique j1, Joueur_Generique j2, Joueur_Generique j_courant, Phase phase_de_jeu){
+	public Etat_de_jeu(Terrain t, Joueur_Generique j1, Joueur_Generique j2, Joueur_Generique j_courant, Phase_Jeu phase_de_jeu){
 		this.terrain = t.clone();
 		this.j1 = j1.clone();
 		this.j2 = j2.clone();
 		this.j_courant = j_courant.clone();
-		this.pdj = phase_de_jeu.clone_Phase();
+		this.pdj = phase_de_jeu;
 	}
 	public Etat_de_jeu()
 	{
@@ -37,7 +37,7 @@ public class Etat_de_jeu {
 	{
 		return j_courant.clone();
 	}
-	public Phase getPdj() {
+	public Phase_Jeu getPdj() {
 		return pdj;
 	}
 	public Etat_de_jeu clone()
@@ -47,7 +47,13 @@ public class Etat_de_jeu {
 		retour.j2 = this.j2.clone();
 		retour.j_courant = this.j_courant.clone();
 		retour.terrain = this.terrain.clone();
-		retour.pdj = this.pdj.clone_Phase();
+		retour.pdj = this.pdj;
 		return retour;
+	}
+	
+	public void afficher()
+	{
+		System.out.println(this.pdj);
+		this.terrain.afficher();
 	}
 }
