4151208980dfa5889cd5d22ca80a9096e80fc164
lavvsont <lavvsont@127.0.0.1>
2016-05-30 16:54:57 +0200

    Ecouteur des boutons du menu démarrage et des règles

---
+++ Taluva/src/Menu/Ecouteur_boutons_demarrage.java
@@ -0,0 +1,27 @@
+package Menu;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Ecouteur_boutons_demarrage  implements ActionListener {
+	String action;
+	Menu_Demarrage menu;
+	
+	Ecouteur_boutons_demarrage(String action,Menu_Demarrage menu){
+		this.action = action;
+		this.menu = menu;
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		switch (action){
+    		case "Règles" : 
+    			menu.regles();
+    			break;
+    		case "Quitter" :
+    			menu.quitter();
+    			break;
+    		default : 
+    			break;
+		}
+	}
+}
