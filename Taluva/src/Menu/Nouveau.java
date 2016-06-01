d8daaf9aa1a1bf29e0973b8245a07fdf65325a1d
lavvsont <lavvsont@127.0.0.1>
2016-06-01 10:06:44 +0200

    Positionnement des boutons et de la fenêtre de nouvelle partie effectué

--- Taluva/src/Menu/Nouveau.java
+++ Taluva/src/Menu/Nouveau.java
@@ -21,22 +21,17 @@ public class Nouveau extends JComponent {
 
 	private JFrame m_fenetre;
 	private JFrame principal;
-	private JPanel central;
-	private JPanel north_in_c;
+	
 	private JLabel joueur_1;
 	private JButton humain_j1;
 	private JButton couleur_j1;
 	
-	private JPanel mid_in_c;
 	private JLabel joueur_2;
 	private JButton humain_j2;
 	private JButton couleur_j2;
 	
-	private JPanel south_in_c;
 	private JButton avance;
 	
-	private JPanel south;
-	private JPanel north_in_s;
 	private JButton accueil;
 	private JButton lancer;
 	
@@ -53,6 +48,7 @@ public class Nouveau extends JComponent {
 		panel.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 		
+		c.insets = new Insets(height_b*3,width_b,0,0);
 		joueur_1 = new JLabel("Joueur 1");
 		joueur_1.setFont(new Font("", Font.BOLD+Font.ITALIC,15));
 		c.weightx = 0.5;
@@ -64,63 +60,79 @@ public class Nouveau extends JComponent {
 		c.ipadx = 0;
 		c.ipady = 0;
 		panel.add(joueur_1,c);
+		c.insets = new Insets(0,0,0,0);
 		
+		c.insets = new Insets(height_b*3,-width_b,0,0);
 		c.gridx = 1;
 		c.gridy = 0;
 		humain_j1 = new JButton("humain _ J1");
 		humain_j1.setFont(new Font("", Font.BOLD+Font.ITALIC,15));
-		humain_j1.setPreferredSize( new Dimension(width_b, height_b) );
+		humain_j1.setPreferredSize( new Dimension(width_b*3/2, height_b) );
 		panel.add(humain_j1,c);
+		c.insets = new Insets(0,0,0,0);
 		
+		c.insets = new Insets(height_b*3,-width_b*2,0,0);
 		c.gridx = 2;
 		c.gridy = 0;
 		couleur_j1 = new JButton("Faction");
 		couleur_j1.setFont(new Font("", Font.BOLD+Font.ITALIC,15));
-		couleur_j1.setPreferredSize( new Dimension(width_b, height_b) );
+		couleur_j1.setPreferredSize( new Dimension(width_b*3/2, height_b) );
 		panel.add(couleur_j1,c);
+		c.insets = new Insets(0,0,0,0);
 		
 		//JOUEUR 2
+		c.insets = new Insets(0,width_b,0,0);
 		c.gridx = 0;
 		c.gridy = 2;
 		joueur_2 = new JLabel("Joueur 2");
 		joueur_2.setFont(new Font("", Font.BOLD+Font.ITALIC,15));
 		panel.add(joueur_2,c);
+		c.insets = new Insets(0,0,0,0);
 		
+		c.insets = new Insets(0,-width_b,0,0);
 		c.gridx = 1;
 		c.gridy = 2;
 		humain_j2 = new JButton("humain _ J2");
 		humain_j2.setFont(new Font("", Font.BOLD+Font.ITALIC,15));
-		humain_j2.setPreferredSize( new Dimension(width_b, height_b) );
+		humain_j2.setPreferredSize( new Dimension(width_b*3/2, height_b) );
 		panel.add(humain_j2,c);
+		c.insets = new Insets(0,0,0,0);
 		
+		c.insets = new Insets(0,-width_b*2,0,0);
 		c.gridx = 2;
 		c.gridy = 2;
 		couleur_j2 = new JButton("Faction");
 		couleur_j2.setFont(new Font("", Font.BOLD+Font.ITALIC,15));
-		couleur_j2.setPreferredSize( new Dimension(width_b, height_b) );
+		couleur_j2.setPreferredSize( new Dimension(width_b*3/2, height_b) );
 		panel.add(couleur_j2,c);
+		c.insets = new Insets(0,0,0,0);
 		
-		//c.insets = new Insets(-50,0,0,0);
+		c.insets = new Insets(0,width_b,0,0);
 		c.gridx = 0;
 		c.gridy = 3;
 		avance = new JButton("Parametres Avancés");
 		avance.setFont(new Font("", Font.BOLD+Font.ITALIC,15));
 		avance.setPreferredSize( new Dimension(width_b*2, height_b) );
 		panel.add(avance,c);
+		c.insets = new Insets(0,0,0,0);
 		
+		c.insets = new Insets(-height_b,width_b*2,0,0);
 		c.gridx = 1;
 		c.gridy = 4;
 		accueil = new JButton("Accueil");
 		accueil.setFont(new Font("", Font.BOLD+Font.ITALIC,15));
-		accueil.setPreferredSize( new Dimension(width_b*9/11, height_b) );
+		accueil.setPreferredSize( new Dimension(width_b, height_b) );
 		panel.add(accueil,c);
+		c.insets = new Insets(0,0,0,0);
 		
+		c.insets = new Insets(-height_b,0,0,0);
 		c.gridx = 2;
 		c.gridy = 4;
 		lancer = new JButton("Lancer");
 		lancer.setFont(new Font("", Font.BOLD+Font.ITALIC,15));
-		lancer.setPreferredSize( new Dimension(width_b*9/11, height_b) );
+		lancer.setPreferredSize( new Dimension(width_b, height_b) );
 		panel.add(lancer,c);
+		c.insets = new Insets(0,0,0,0);
 		
 		m_fenetre.add(panel);
 	
@@ -131,9 +143,8 @@ public class Nouveau extends JComponent {
 		m_fenetre = new JFrame("Nouveau");
 		m_fenetre.setLayout(new BorderLayout());
 		m_fenetre.setVisible(true);
-		m_fenetre.setBackground(Color.WHITE);
-		m_fenetre.setLocation(frame.getWidth()/11, frame.getHeight()/11);
-		m_fenetre.setSize(frame.getWidth()*9/11,frame.getHeight()*9/11);
+		m_fenetre.setLocation(frame.getWidth()*5/22, frame.getHeight()*5/22);
+		m_fenetre.setSize(frame.getWidth()*6/11,frame.getHeight()*6/11);
 		m_fenetre.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		m_fenetre.addWindowListener(new WindowAdapter() {
             @Override
