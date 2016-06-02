20d8c23613725c774f5178c62f8ebce5035f51f5
Dimitri <Dimitri@Dimi-PC>
2016-06-02 15:59:33 +0200

    marking menu

--- Taluva/src/IHM/Menu_circulaire_creation.java
+++ Taluva/src/IHM/Menu_circulaire_creation.java
@@ -27,10 +27,10 @@ import Moteur.Phase.Phase_Jeu;
 
 public class Menu_circulaire_creation {
 	Moteur moteur;
-	ButtonConstruction bouton_hute,bouton_tour,bouton_temple;
+	ButtonConstruction bouton_huteC,bouton_tourC,bouton_templeC,bouton_huteV,bouton_tourV,bouton_templeV,bouton_huteE,bouton_tourE,bouton_templeE,bouton_huteB,bouton_tourB,bouton_templeB;
 	ButtonExtension bouton_foret,bouton_plage,bouton_montagne,bouton_plaine,bouton_lac;
 	Grid grille;
-	Drawable drawable;
+	Drawable drawableC,drawableV,drawableB,drawableE;
 	Drawable drawableExtension;
 	float rayon;
 	private static boolean draw = false;
@@ -40,9 +40,21 @@ public class Menu_circulaire_creation {
 	
 	public Menu_circulaire_creation(Moteur moteur,Loader loader,GraphicConstruction Construction,Grid grille,Camera camera){
 		this.moteur=moteur;
-		bouton_hute = new ButtonConstruction(loader.loadTexture("Button_Hut.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),"hut",Construction,moteur);
-		bouton_tour = new ButtonConstruction(loader.loadTexture("Button_tower.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),"tower",Construction,moteur);
-		bouton_temple = new ButtonConstruction(loader.loadTexture("Button_Temple.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),"temple",Construction,moteur);
+		bouton_huteC = new ButtonConstruction(loader.loadTexture("Icons/Chinese/Hut.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),"hut",Construction,moteur);
+		bouton_tourC = new ButtonConstruction(loader.loadTexture("Icons/Chinese/Tower.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),"tower",Construction,moteur);
+		bouton_templeC = new ButtonConstruction(loader.loadTexture("Icons/Chinese/Temple.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),"temple",Construction,moteur);
+		
+		bouton_huteV = new ButtonConstruction(loader.loadTexture("Icons/Viking/Hut.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),"hut",Construction,moteur);
+		bouton_tourV = new ButtonConstruction(loader.loadTexture("Icons/Viking/Tower.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),"tower",Construction,moteur);
+		bouton_templeV = new ButtonConstruction(loader.loadTexture("Icons/Viking/Temple.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),"temple",Construction,moteur);
+		
+		bouton_huteE = new ButtonConstruction(loader.loadTexture("Icons/Europe/Hut.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),"hut",Construction,moteur);
+		bouton_tourE = new ButtonConstruction(loader.loadTexture("Icons/Europe/Tower.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),"tower",Construction,moteur);
+		bouton_templeE = new ButtonConstruction(loader.loadTexture("Icons/Europe/Temple.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),"temple",Construction,moteur);
+		
+		bouton_huteB = new ButtonConstruction(loader.loadTexture("Icons/Babylon/Hut.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),"hut",Construction,moteur);
+		bouton_tourB = new ButtonConstruction(loader.loadTexture("Icons/Babylon/Tower.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),"tower",Construction,moteur);
+		bouton_templeB = new ButtonConstruction(loader.loadTexture("Icons/Babylon/Temple.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),"temple",Construction,moteur);
 		
 		bouton_foret = new ButtonExtension(loader.loadTexture("Tile/F.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),Case.Type.FORET,moteur,Construction);
 		bouton_plage = new ButtonExtension(loader.loadTexture("Tile/S.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),Case.Type.SABLE,moteur,Construction);
@@ -50,11 +62,26 @@ public class Menu_circulaire_creation {
 		bouton_plaine = new ButtonExtension(loader.loadTexture("Tile/P.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),Case.Type.PLAINE,moteur,Construction);
 		bouton_lac = new ButtonExtension(loader.loadTexture("Tile/L.png"),new Vector2f(Display.getWidth()-150,50),new Vector2f(36,36),Case.Type.LAC,moteur,Construction);
 		this.grille=grille;
-		drawable=new Drawable(loader);
+		drawableC=new Drawable(loader);
+		drawableB=new Drawable(loader);
+		drawableV=new Drawable(loader);
+		drawableE=new Drawable(loader);
 		drawableExtension = new Drawable(loader);
-		drawable.bindButton(bouton_hute);
-		drawable.bindButton(bouton_tour);
-		drawable.bindButton(bouton_temple);
+		drawableC.bindButton(bouton_huteC);
+		drawableC.bindButton(bouton_tourC);
+		drawableC.bindButton(bouton_templeC);
+		
+		drawableB.bindButton(bouton_huteB);
+		drawableB.bindButton(bouton_tourB);
+		drawableB.bindButton(bouton_templeB);
+		
+		drawableV.bindButton(bouton_huteV);
+		drawableV.bindButton(bouton_tourV);
+		drawableV.bindButton(bouton_templeV);
+		
+		drawableE.bindButton(bouton_huteE);
+		drawableE.bindButton(bouton_tourE);
+		drawableE.bindButton(bouton_templeE);
 
 		drawableExtension.bindButton(bouton_foret);
 		drawableExtension.bindButton(bouton_plage);
@@ -72,9 +99,21 @@ public class Menu_circulaire_creation {
 		if(camera.getDistanceFromPivot()<50){
 			distance = 50;
 		}
-			bouton_hute.setDimension(new Vector2f(50/(distance/ratio),50/(distance/ratio)));
-			bouton_tour.setDimension(new Vector2f(50/(distance/ratio),50/(distance/ratio)));
-			bouton_temple.setDimension(new Vector2f(50/(distance/ratio),50/(distance/ratio)));
+			bouton_huteC.setDimension(new Vector2f(50/(distance/ratio),50/(distance/ratio)));
+			bouton_tourC.setDimension(new Vector2f(50/(distance/ratio),50/(distance/ratio)));
+			bouton_templeC.setDimension(new Vector2f(50/(distance/ratio),50/(distance/ratio)));
+			
+			bouton_huteV.setDimension(new Vector2f(50/(distance/ratio),50/(distance/ratio)));
+			bouton_tourV.setDimension(new Vector2f(50/(distance/ratio),50/(distance/ratio)));
+			bouton_templeV.setDimension(new Vector2f(50/(distance/ratio),50/(distance/ratio)));
+			
+			bouton_huteB.setDimension(new Vector2f(50/(distance/ratio),50/(distance/ratio)));
+			bouton_tourB.setDimension(new Vector2f(50/(distance/ratio),50/(distance/ratio)));
+			bouton_templeB.setDimension(new Vector2f(50/(distance/ratio),50/(distance/ratio)));
+			
+			bouton_huteE.setDimension(new Vector2f(50/(distance/ratio),50/(distance/ratio)));
+			bouton_tourE.setDimension(new Vector2f(50/(distance/ratio),50/(distance/ratio)));
+			bouton_templeE.setDimension(new Vector2f(50/(distance/ratio),50/(distance/ratio)));
 			
 			bouton_foret.setDimension(new Vector2f(50/(distance/ratio),50/(distance/ratio)));
 			bouton_plage.setDimension(new Vector2f(50/(distance/ratio),50/(distance/ratio)));
@@ -106,19 +145,61 @@ public class Menu_circulaire_creation {
 				draw = true;
 				ButtonConstruction.setPoint(new Point(i,j));
 				//etat bouton hutte
-				bouton_hute.setGrey(!(moteur.getTerrain().placement_batiment_autorise(Type_Batiment.HUTTE,moteur.get_Jcourant().getCouleur() , coord.indices) && moteur.get_Jcourant().getHutte()>0));
+				bouton_huteC.setGrey(!(moteur.getTerrain().placement_batiment_autorise(Type_Batiment.HUTTE,moteur.get_Jcourant().getCouleur() , coord.indices) && moteur.get_Jcourant().getHutte()>0));
 				//gerer la position du bouton hutte
-				bouton_hute.setPosition(new Vector2f(Mouse.getX()-rayon/2,Display.getHeight()- Mouse.getY()-rayon/2-rayon));
+				bouton_huteC.setPosition(new Vector2f(Mouse.getX()-rayon/2,Display.getHeight()- Mouse.getY()-rayon/2-rayon));
 				
 				//etat bouton temple
-				bouton_temple.setGrey(!(moteur.getTerrain().placement_batiment_autorise(Type_Batiment.TEMPLE,moteur.get_Jcourant().getCouleur() , coord.indices) && moteur.get_Jcourant().getTemple()>0));
+				bouton_templeC.setGrey(!(moteur.getTerrain().placement_batiment_autorise(Type_Batiment.TEMPLE,moteur.get_Jcourant().getCouleur() , coord.indices) && moteur.get_Jcourant().getTemple()>0));
 				//gerer la position du bouton temple
-				bouton_temple.setPosition(new Vector2f(Mouse.getX()-rayon/2-rayon*(float) Math.sin(Math.PI*2f/3f),Display.getHeight()- Mouse.getY()-rayon/2-rayon*(float) Math.cos(Math.PI*2f/3f)));
+				bouton_templeC.setPosition(new Vector2f(Mouse.getX()-rayon/2-rayon*(float) Math.sin(Math.PI*2f/3f),Display.getHeight()- Mouse.getY()-rayon/2-rayon*(float) Math.cos(Math.PI*2f/3f)));
 				
 				//etat bouton tour
-				bouton_tour.setGrey(!(moteur.getTerrain().placement_batiment_autorise(Type_Batiment.TOUR,moteur.get_Jcourant().getCouleur() , coord.indices) && moteur.get_Jcourant().getTour()>0));
+				bouton_tourC.setGrey(!(moteur.getTerrain().placement_batiment_autorise(Type_Batiment.TOUR,moteur.get_Jcourant().getCouleur() , coord.indices) && moteur.get_Jcourant().getTour()>0));
 				//gerer la position du bouton tour
-				bouton_tour.setPosition(new Vector2f(Mouse.getX()-rayon/2-rayon*(float) Math.sin(Math.PI*4f/3f),Display.getHeight()- Mouse.getY()-rayon/2-rayon*(float) Math.cos(Math.PI*4f/3f)));
+				bouton_tourC.setPosition(new Vector2f(Mouse.getX()-rayon/2-rayon*(float) Math.sin(Math.PI*4f/3f),Display.getHeight()- Mouse.getY()-rayon/2-rayon*(float) Math.cos(Math.PI*4f/3f)));
+
+				bouton_huteV.setGrey(!(moteur.getTerrain().placement_batiment_autorise(Type_Batiment.HUTTE,moteur.get_Jcourant().getCouleur() , coord.indices) && moteur.get_Jcourant().getHutte()>0));
+				//gerer la position du bouton hutte
+				bouton_huteV.setPosition(new Vector2f(Mouse.getX()-rayon/2,Display.getHeight()- Mouse.getY()-rayon/2-rayon));
+				
+				//etat bouton temple
+				bouton_templeV.setGrey(!(moteur.getTerrain().placement_batiment_autorise(Type_Batiment.TEMPLE,moteur.get_Jcourant().getCouleur() , coord.indices) && moteur.get_Jcourant().getTemple()>0));
+				//gerer la position du bouton temple
+				bouton_templeV.setPosition(new Vector2f(Mouse.getX()-rayon/2-rayon*(float) Math.sin(Math.PI*2f/3f),Display.getHeight()- Mouse.getY()-rayon/2-rayon*(float) Math.cos(Math.PI*2f/3f)));
+				
+				//etat bouton tour
+				bouton_tourV.setGrey(!(moteur.getTerrain().placement_batiment_autorise(Type_Batiment.TOUR,moteur.get_Jcourant().getCouleur() , coord.indices) && moteur.get_Jcourant().getTour()>0));
+				//gerer la position du bouton tour
+				bouton_tourV.setPosition(new Vector2f(Mouse.getX()-rayon/2-rayon*(float) Math.sin(Math.PI*4f/3f),Display.getHeight()- Mouse.getY()-rayon/2-rayon*(float) Math.cos(Math.PI*4f/3f)));
+	
+				bouton_huteE.setGrey(!(moteur.getTerrain().placement_batiment_autorise(Type_Batiment.HUTTE,moteur.get_Jcourant().getCouleur() , coord.indices) && moteur.get_Jcourant().getHutte()>0));
+				//gerer la position du bouton hutte
+				bouton_huteE.setPosition(new Vector2f(Mouse.getX()-rayon/2,Display.getHeight()- Mouse.getY()-rayon/2-rayon));
+				
+				//etat bouton temple
+				bouton_templeE.setGrey(!(moteur.getTerrain().placement_batiment_autorise(Type_Batiment.TEMPLE,moteur.get_Jcourant().getCouleur() , coord.indices) && moteur.get_Jcourant().getTemple()>0));
+				//gerer la position du bouton temple
+				bouton_templeE.setPosition(new Vector2f(Mouse.getX()-rayon/2-rayon*(float) Math.sin(Math.PI*2f/3f),Display.getHeight()- Mouse.getY()-rayon/2-rayon*(float) Math.cos(Math.PI*2f/3f)));
+				
+				//etat bouton tour
+				bouton_tourE.setGrey(!(moteur.getTerrain().placement_batiment_autorise(Type_Batiment.TOUR,moteur.get_Jcourant().getCouleur() , coord.indices) && moteur.get_Jcourant().getTour()>0));
+				//gerer la position du bouton tour
+				bouton_tourE.setPosition(new Vector2f(Mouse.getX()-rayon/2-rayon*(float) Math.sin(Math.PI*4f/3f),Display.getHeight()- Mouse.getY()-rayon/2-rayon*(float) Math.cos(Math.PI*4f/3f)));
+	
+				bouton_huteB.setGrey(!(moteur.getTerrain().placement_batiment_autorise(Type_Batiment.HUTTE,moteur.get_Jcourant().getCouleur() , coord.indices) && moteur.get_Jcourant().getHutte()>0));
+				//gerer la position du bouton hutte
+				bouton_huteB.setPosition(new Vector2f(Mouse.getX()-rayon/2,Display.getHeight()- Mouse.getY()-rayon/2-rayon));
+				
+				//etat bouton temple
+				bouton_templeB.setGrey(!(moteur.getTerrain().placement_batiment_autorise(Type_Batiment.TEMPLE,moteur.get_Jcourant().getCouleur() , coord.indices) && moteur.get_Jcourant().getTemple()>0));
+				//gerer la position du bouton temple
+				bouton_templeB.setPosition(new Vector2f(Mouse.getX()-rayon/2-rayon*(float) Math.sin(Math.PI*2f/3f),Display.getHeight()- Mouse.getY()-rayon/2-rayon*(float) Math.cos(Math.PI*2f/3f)));
+				
+				//etat bouton tour
+				bouton_tourB.setGrey(!(moteur.getTerrain().placement_batiment_autorise(Type_Batiment.TOUR,moteur.get_Jcourant().getCouleur() , coord.indices) && moteur.get_Jcourant().getTour()>0));
+				//gerer la position du bouton tour
+				bouton_tourB.setPosition(new Vector2f(Mouse.getX()-rayon/2-rayon*(float) Math.sin(Math.PI*4f/3f),Display.getHeight()- Mouse.getY()-rayon/2-rayon*(float) Math.cos(Math.PI*4f/3f)));
 	
 				
 			}
@@ -179,10 +260,26 @@ public class Menu_circulaire_creation {
 		}
 		
 		if(draw){
-			bouton_hute.update();
-			bouton_temple.update();
-			bouton_tour.update();
-			drawable.draw();
+			bouton_huteC.update();
+			bouton_templeC.update();
+			bouton_tourC.update();
+			switch(moteur.get_Jcourant().getCouleur()){
+			case JAUNE:
+				drawableC.draw();
+				break;
+			case BLEU:
+				drawableE.draw();
+				break;
+			case ROSE:
+				drawableV.draw();
+				break;
+			case BLANC:
+				drawableB.draw();
+				break;
+			default:
+				break;
+			}
+			
 		}
 		if(drawExtension){
 			bouton_foret.update();
@@ -195,7 +292,10 @@ public class Menu_circulaire_creation {
 	}
 	
 	public void cleanUp(){
-		drawable.cleanUp();
+		drawableC.cleanUp();
+		drawableB.cleanUp();
+		drawableV.cleanUp();
+		drawableE.cleanUp();
 		drawableExtension.cleanUp();
 	}
 	
