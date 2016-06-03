3fab3ef3955f3c4c4443280b5b0ada3c27f66018
lavvsont <lavvsont@127.0.0.1>
2016-06-03 10:46:16 +0200

    modif

--- Taluva/src/charger_sauvegarder/Sauvegarde.java
+++ Taluva/src/charger_sauvegarder/Sauvegarde.java
@@ -5,19 +5,15 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
-import test.Game;
 import Moteur.Moteur;
 
 public class Sauvegarde {
-	private Game game;
 	private Moteur moteur;
-	public Sauvegarde(Game game,Moteur moteur){
-		this.game=game;
+	public Sauvegarde(Moteur moteur){
 		this.moteur=moteur;
 	}
-	public void restore(Game game,Moteur moteur){
+	public void restore(Moteur moteur){
 		moteur=this.moteur;
-		game=this.game;
 	}
 	
 	public void sauvegarder(Sauvegarde save,String path){
